aa09e0ac4af9b472a03711f43f63fe8076724d5d
CyberMan010 <mohamahasoun60@gmail.com>
2024-06-26 23:59:28 +0300

    hype

---
+++ script.js
@@ -0,0 +1,50 @@
+// let x = 1;
+// let y = 2;
+// function sum() {
+//     result = x - y;
+//     return result;
+// }
+// let summary = sum();
+
+
+// class Stack {
+//     constructor() {
+//         this.top = 1;
+//         this.array = new Array();
+//         this.count = summary;
+//     }
+//     return() {
+//         return this.array;
+//     }
+//     push(value) {
+        
+//         this.count++;
+//         this.array[this.count] = value;
+//     }
+//     pop(){
+//         if (this.array.length == -1) {
+//             console.log("stack is mty");
+//         } else {
+//             let val = this.array[this.count]
+//             this.count--;
+//             return val;
+//         }
+// }
+// }
+// function reverse(array) {
+//     let reversearray = [];
+//     let stack = new Stack();
+//     for (let i = 0; i < array.length; i++){
+//         stack.push(array[i]);
+        
+//     }
+//     for (let i = 0; i < array.length; i++){
+//         reversearray.push(stack.pop());
+//     }
+//     return reversearray;
+
+// }
+// const arr = new Stack(10);
+// console.log(reverse([1,2,3,4,5]));
+
+
